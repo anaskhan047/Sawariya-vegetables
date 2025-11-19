74a4af7e2c0445de8e8f7ee72f511d041db9a2d4
anaskhan047 <anaskhany47@gmail.com>
2025-11-19 14:45:43 +0530

    anas

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -2,7 +2,6 @@
 import React, { useState, Suspense } from "react";
 import ShopSidebarWrapper from "../components/shop/ShopSidebarWrapper";
 import ShopGridWrapper from "../components/shop/ShopGridWrapper";
-import { Metadata } from "next";
 
 export default function ShopPage() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
